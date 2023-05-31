57fbaf6b3434a175e865b5a669fa55ee56e6aa35
Josh <joshuastolle445@gmail.com>
2023-05-31 16:51:26 +0200

    Initial commit

---
+++ src/main/java/Controlflow.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Controlflow {
+
+    public static String hasTooManyPeople(int numberOfPeople) {
+        if (numberOfPeople > 30) {
+            return "too many people!";
+        } else if (numberOfPeople > 30) {
+            return "not too many people!";
+        } else {
+            return "not too many people, but almost!";
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many people in the shop? type and hit ENTER:");
+        int input = Integer.parseInt(scanner.nextLine());
+        System.out.println("In the shop are " + hasTooManyPeople(input));
+
+    }
+}
